76ac3e4105fd8d7b686380cb303755e4f8fdae80
blackhatjohnny <ogwurujohnson@gmail.com>
2018-08-29 11:19:05 +0100

    refactored codes to fit Air-bnb eslint style guide

--- api/controllers/question.js
+++ api/controllers/question.js
@@ -1,27 +1,32 @@
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const client = require('../models/db');
 
-
-exports.getAllQuestions = (req,res)=>{
-    res.json({'message': 'All Questions'});
+exports.getAllQuestions = (req, res) => {
+  const query = client.query('SELECT * from questions;');
+  query.on('row', (row, result) => {
+    result.addRow(row);
+  });
+  query.on('end', (result) => {
+    const jsonString = JSON.stringify(result.rows);
+    const jsonObj = JSON.parse(jsonString);
+    res.send(jsonString);
+    client.end();
+    context.succeed(jsonObj);
+  });
 };
 
-exports.getSingleQuestion = (req,res)=>{
-    res.json({'message': 'Single Question'});
+exports.getSingleQuestion = (req, res) => {
+  res.json({ message: 'Single Question' });
 };
 
-exports.editQuestion = (req,res)=>{
-    res.json({'message': 'edit Question'});
+exports.editQuestion = (req, res) => {
+  res.json({ message: 'edit Question' });
 };
 
-exports.deleteQuestion = (req,res)=>{
-    res.json({'message': 'delete question'});
+exports.deleteQuestion = (req, res) => {
+  res.json({ message: 'delete question' });
 };
 
-exports.postQuestion = (req,res)=>{
-    res.json({'message': 'ask question'});
+exports.postQuestion = (req, res) => {
+  res.json({ message: 'ask question' });
 };
-
-
-
-
-
