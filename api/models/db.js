af049bae4c2c3b8c9a204c9f6387677c9c0c6737
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-01 12:33:39 +0100

    setup db and env configuration

--- api/models/db.js
+++ api/models/db.js
@@ -1,29 +1,15 @@
-/* const pg = require('pg');
+const pg = require('pg');
+require('dotenv').config();
 
-const conn = 'postgres://stack_user:Johnny55@localhost/stackoverflow';
-
-const client = new pg.Client(conn);
-client.connect();
-console.log('Connected to postgreSQL database');
-
-module.exports = client; */
-
-/* const config = {
-  user: 'stack_user',
-  database: 'stackoverflow',
-  password: 'Johnny55',
-  port: 5432,
-  max: 10,
+const config = {
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000,
 };
+
 const pool = new pg.Pool(config);
 
-pool.connect((err, client, done) => {
-  if (err) {
-    console.log(`not able to get connection ${err}`);
-  } else {
-    console.log('connection established');
-  }
-});
-
-module.exports = pool; */
+module.exports = pool;
