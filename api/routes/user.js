ca6ff42b9a37322cadc1036f1b3fce5ec6c42e61
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-04 07:33:15 +0100

    generating token for sign in user

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/user');
 
 // user auth route
 router.post('/signup', userController.addUser);
+router.post('/login', userController.loginUser);
 
 
 // user routes
