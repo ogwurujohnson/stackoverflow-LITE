01d48877027e400081c748e29cc2ac61449eb94e
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-04 08:40:57 +0100

    added comment to authorization check script

--- api/middleware/verify_auth.js
+++ api/middleware/verify_auth.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 module.exports = (req, res, next) => {
   try {
+    // fetch token from header
     const token = req.headers.authorization.split(' ')[1];
+    // use jwt.verify to verify sent token
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.userData = decoded;
     next();
