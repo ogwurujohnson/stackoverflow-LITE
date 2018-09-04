07addd6e896ae9864a3da7ad6b9258a7c1f8a753
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-04 08:36:55 +0100

    updated routes with the middleware to check for auth

--- api/routes/question.js
+++ api/routes/question.js
@@ -24,22 +24,22 @@ router.delete('/:q_id/delete', questionController.deleteQuestion);
 
 router.get('/:q_id/edit', questionController.editQuestion);
 router.put('/:q_id/edit', questionController.editQuestion);
-
+const checkAuth = require('../middleware/verify_auth');
 
 /**
  * Routes to handle answers
  */
-router.post('/:q_id/answers', answerController.postAnswer);
+router.post('/:q_id/answers', checkAuth, answerController.postAnswer);
 router.get('/:q_id/answers', answerController.getQuestionAnswers);
 
-router.put('/:q_id/answers/:a_id/edit', answerController.editAnswer);
-router.delete('/:q_id/answers/:a_id/delete', answerController.deleteAnswer);
+router.put('/:q_id/answers/:a_id/edit', checkAuth, answerController.editAnswer);
+router.delete('/:q_id/answers/:a_id/delete', checkAuth, answerController.deleteAnswer);
 
-router.post('/:q_id/answers/:a_id/reply', answerController.replyAnswer);
+router.post('/:q_id/answers/:a_id/reply', checkAuth, answerController.replyAnswer);
 router.get('/:q_id/answers/:a_id/reply', answerController.getAllReply);
 
-router.put('/:q_id/answers/:a_id/upvote', answerController.upVoteAnswer);
-router.put('/:q_id/answers/:a_id/downvote', answerController.downVoteAnswer);
+router.put('/:q_id/answers/:a_id/upvote', checkAuth, answerController.upVoteAnswer);
+router.put('/:q_id/answers/:a_id/downvote', checkAuth, answerController.downVoteAnswer);
 
 
 module.exports = router;
