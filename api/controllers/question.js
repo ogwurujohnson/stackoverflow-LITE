a3332a44b2666ac11339b0f6e099f806f3baec3b
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-05 17:24:08 +0100

    working on deleting a question

--- api/controllers/question.js
+++ api/controllers/question.js
@@ -1,5 +1,10 @@
-// const bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const pool = require('../models/db');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 exports.getAllQuestions = (req, res) => {
   pool.connect((err, client, done) => {
@@ -37,13 +42,43 @@ exports.editQuestion = (req, res) => {
 };
 
 exports.deleteQuestion = (req, res) => {
-  res.json({ message: 'delete question' });
+  // DELETE FROM tbltravellers WHERE memo_serial='$pnr'
+  pool.connect((err, client, done) => {
+    const data = {
+      questionId: req.params.q_id,
+    };
+
+    const query = 'DELETE FROM questions WHERE question_id = $1 ';
+    const values = [data.questionId];
+    client.query(query, values, (error, result) => {
+      done();
+      if (error) {
+        console.log(error);
+        res.status(400).send(error);
+      }
+      res.status(200).json(result);
+    });
+  });
 };
 
 exports.postQuestion = (req, res) => {
-  const data = {
-    title: req.body.title,
-    description: req.body.description,
-  };
-  res.json({ message: 'ask question' });
+  pool.connect((err, client, done) => {
+    const data = {
+      title: req.body.title,
+      description: req.body.description,
+      user: req.body.user,
+    };
+
+    const query = 'INSERT INTO questions(question_title,question_description,user_id) VALUES($1, $2, $3) RETURNING *';
+    const values = [data.title, data.description, data.user];
+
+    client.query(query, values, (error, result) => {
+      done();
+      if (error) {
+        console.log(error);
+        res.status(400).send(error);
+      }
+      res.status(200).json(data);
+    });
+  });
 };
