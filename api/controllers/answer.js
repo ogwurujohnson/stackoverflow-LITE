2c5dbf6274e41195ab1e58ab2a719c6a417b8e6f
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-05 18:06:50 +0100

    users can post answer to question

--- api/controllers/answer.js
+++ api/controllers/answer.js
@@ -2,11 +2,25 @@
 const pool = require('../models/db');
 
 exports.postAnswer = (req, res) => {
-  const questionId = req.params.q_id;
-  const data = {
-    description: req.body.description,
-    
-  };
+  pool.connect((err, client, done) => {
+    const data = {
+      questionId: req.params.q_id,
+      description: req.body.description,
+      userId: req.body.userId,
+    };
+
+    const query = 'INSERT INTO answers(question_id,answer_description, user_id) VALUES($1, $2, $3) RETURNING *';
+    const values = [data.questionId, data.description, data.userId];
+
+    client.query(query, values, (error, result) => {
+      done();
+      if (error) {
+        console.log(error);
+        res.status(400).send(error);
+      }
+      res.status(200).json(data);
+    });
+  });
 };
 
 exports.getQuestionAnswers = (req, res) => {
@@ -24,6 +38,21 @@ exports.getQuestionAnswers = (req, res) => {
 };
 
 exports.editAnswer = (req, res) => {
+  const answerId = req.params.a_id;
+  const data = {
+    description: req.body.description,
+    userId: req.body.userId,
+  };
+  pool.connect((err, client, done) => {
+    client.query('UPDATE questions SET answer_description=$2,user_id=$3 Where answer_id = $1', [answerId, data.description, data.userId], (error, result) => {
+      done();
+      if (error) {
+        console.log(error);
+        res.status(400).send(error);
+      }
+      res.status(200).json(data);
+    });
+  });
   res.json({ message: 'edit answer' });
 };
 
@@ -39,7 +68,7 @@ exports.getAllReply = (req, res) => {
   const answerId = req.params.a_id;
   const questionId = req.params.q_id;
   pool.connect((err, client, done) => {
-    client.query('SELECT * FROM answers WHERE answer_id = $1 && question_id = $2', [answerId,questionId], (error, result) => {
+    client.query('SELECT * FROM answers WHERE answer_id = $1 && question_id = $2', [answerId, questionId], (error, result) => {
       done();
       if (error) {
         console.log(error);
