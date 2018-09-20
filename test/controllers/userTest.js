8c069a3dc9f9d11dc182114418d12f92217598b6
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-20 09:27:50 +0100

    update to user test suite

--- test/controllers/userTest.js
+++ test/controllers/userTest.js
@@ -1,5 +1,5 @@
-/* const chai = require('chai');
-const { expect } = require('chai');
+const chai = require('chai');
+const should = require('chai').should();
 
 chai.use(require('chai-http'));
 
@@ -8,53 +8,53 @@ const users = require('../testInfo'); // test users
 
 let personalToken;
 
-describe('GET api/v1/users', () => {
-  it('should return response 200', (done) => {
-    chai.request(app)
-      .get('/api/v1/users')
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res).to.have.status(200);
-        expect(res).to.be.an('object');
-        done();
-      });
+describe('USERS', () => {
+  describe('GET api/v1/users', () => {
+    it('should return response 200', (done) => {
+      chai.request(app)
+        .get('/api/v1/users')
+        .end((err, res) => {
+          if (err) done(err);
+          res.should.has.status(200);
+          res.should.be.a('object');
+          done();
+        });
+    });
+    it('should return an array of users', (done) => {
+      chai.request(app)
+        .get('/api/v1/users')
+        .end((err, res) => {
+          if (err) done(err);
+          res.body.should.have.property('users');
+          res.body.users.should.be.a('array');
+          done();
+        });
+    });
+  });
+  describe('GET api/v1/users/:u_id', () => {
+    it('should return response status 200', (done) => {
+      chai.request(app)
+        .get('/api/v1/users/1')
+        .end((err, res) => {
+          if (err) done(err);
+          res.should.have.status(200);
+          res.should.be.a('object');
+          done();
+        });
+    });
+    it('it should return a single user', (done) => {
+      chai.request(app)
+        .get('/api/v1/users/1')
+        .end((err, res) => {
+          if (err) done(err);
+          res.body.should.have.property('user');
+          res.body.should.have.property('questions');
+          res.body.user.should.be.a('array');
+          res.body.questions.should.be.a('array');
+          done();
+        });
+    });
   });
 
-  it('should return an array of users', (done) => {
-    chai.request(app)
-      .get('/api/v1/users')
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res.body).to.haveOwnProperty('users');
-        expect(res.body.users).to.be.an('array');
-        done();
-      });
-  });
+  
 });
-
-
-describe('GET api/v1/users/:u_id', () => {
-  it('should return response status 200', (done) => {
-    chai.request(app)
-      .get('/api/v1/users/1')
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res).to.have.status(200);
-        expect(res).to.be.an('object');
-        done();
-      });
-  });
-
-  it('it should return a single user', (done) => {
-    chai.request(app)
-      .get('/api/v1/users/1')
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res.body).to.haveOwnProperty('user');
-        expect(res.body).to.haveOwnProperty('questions');
-        expect(res.body.user).to.be.an('array');
-        expect(res.body.questions).to.be.an('array');
-        done();
-      });
-  });
-}); */
