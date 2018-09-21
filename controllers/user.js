e9b7c83cbf4c5e5ab5f31f7e051cc1e84b70bbf9
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-21 22:17:09 +0100

    refactoring user controller to depend on the dbhelper

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pool = require('../models/db');
+const { getSingle,getAll } = require('../models/dbHelper');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,49 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // users controller
 exports.getAllUsers = (req, res) => {
-  pool.connect((err, client, done) => {
-    if (err) {
-      console.log(`Can not connect to the DB  ${err}`);
-    }
-    console.log('connected to database');
-    client.query('SELECT * FROM users', (error, result) => {
-      done();
-      if (err) {
-        console.log(error);
-        res.status(400).send(error);
-      }
-      res.status(200).json({
-        status: 'success',
-        message: 'users fetched successfully',
-        users: result.rows,
-      });
-    });
-  });
+  getAll('users', req, res);
 };
 
 exports.getUserDetails = (req, res) => {
-  pool.connect((err, client, done) => {
-    const userId = req.params.u_id;
-    client.query('SELECT user_id,firstname,lastname,email FROM users WHERE user_id = $1', [userId], (error, result) => {
-      done();
-      if (error) {
-        res.status(400).send(error);
-      }
-      const fetchedUser = result.rows[0].user_id;
-      client.query('SELECT * FROM questions WHERE user_id = $1', [fetchedUser], (error, answers) => {
-        done();
-        if (error) {
-          res.status(400).send(error);
-        }
-        res.status(200).json({
-          status: 'success',
-          message: 'user fetched successfully',
-          user: result.rows,
-          questions: answers.rows,
-        });
-      });
-    });
-  });
+  const userId = req.params.u_id;
+  getSingle('users', userId, 'user_id', req, res);
 };
 
 // users question controller
