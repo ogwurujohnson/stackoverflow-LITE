c619855e7cf3b6c4dfb0f3d2dfcdf4f85de576bd
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-24 15:20:23 +0100

    update to question controller

--- controllers/question.js
+++ controllers/question.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pool = require('../models/db');
-const { getAll, getSingle, editQuestion, deleteResource } = require('../models/dbHelper');
+const {
+  getAll, getSingle, editQuestion, deleteResource, postQuestion,
+} = require('../models/dbHelper');
 
 const app = express();
 app.use(bodyParser.json());
@@ -38,24 +40,11 @@ exports.deleteQuestion = (req, res) => {
 };
 
 exports.postQuestion = (req, res) => {
-  pool.connect((err, client, done) => {
-    const data = {
-      title: req.body.title,
-      description: req.body.description,
-      userId: req.body.userId,
-    };
+  const data = {
+    title: req.body.title,
+    description: req.body.description,
+    userId: req.body.userId,
+  };
 
-    const query = 'INSERT INTO questions(question_title,question_description,user_id) VALUES($1, $2, $3) RETURNING *';
-    const values = [data.title, data.description, data.userId];
-
-    client.query(query, values, (error, result) => {
-      done();
-      if (error) {
-        res.status(400).send(error);
-      }
-      res.status(201).json({
-        newQuestion: data,
-      });
-    });
-  });
+  postQuestion(data, res);
 };
