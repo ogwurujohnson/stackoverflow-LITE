f183ade82045a670afc0db0add8645c8a0277ff7
blackhatjohnny <ogwurujohnson@gmail.com>
2018-09-27 11:08:22 +0100

    update to test info

---
+++ tests/testInfo.js
@@ -0,0 +1,16 @@
+const users = {
+  patrick: {
+    email: 'test@gmail.com',
+    password: 'test',
+  },
+  johnson: {
+    email: 'ogwurujohnson@gmail.com',
+    password: 'Johnny55',
+  },
+  paddy: {
+    email: 'ogwurupatrick@gmail.com',
+    password: 'Johnny55',
+  },
+};
+
+module.exports = users;
