da96405f0af4a8f0bbe596cb4b71a4c2b515d715
blackhatjohnny <ogwurujohnson@gmail.com>
2018-10-02 11:36:37 +0100

    added cors

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const authRouter = require('./routes/auth');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get('/api/v1', (req, res) => {
   res.send('Welcome to the stackoverflow-lite API');
@@ -57,12 +58,12 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); //* will allow from all cross domain
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
-});
+}); */
 
 const port = process.env.port || 3000;
 
